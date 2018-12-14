83c01ad929fb040c8e4a45880a4746cb48103abe
Michael Elvers <melvers@gmail.com>
2018-12-14 22:47:24 +0100

    Use of command line options

--- thebook/src/program/Program.java
+++ thebook/src/program/Program.java
@@ -9,6 +9,12 @@ import javax.xml.parsers.*;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactoryConfigurationError;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.MissingOptionException;
+import org.apache.commons.cli.Options;
 import org.xml.sax.SAXException;
 
 import helper.Misc;
@@ -18,6 +24,19 @@ import helper.Container;
 // Table from https://github.com/iamcal/emoji-data
 public class Program {
 
+	public static Options getOptions() {
+		Options options = new Options();
+		options.addRequiredOption("i", "inputdir", true,
+				"Input directory, should contain a subdirectory 'chat' where conversion and images is stored");
+		options.addRequiredOption("e", "emojidir", true, "Directory where the emoji png images are stored");
+		options.addOption("o", "outputdir", true, "Output directory, default is input directory");
+		options.addOption("imagepooldir", true,
+				"Directory of the image pool. Only used when 'media omitted' messages are found");
+		options.addOption("debugdir",true,"Directory where debug information is stored");
+		
+		return options;
+	}
+
 	/**
 	 * @param args
 	 * @throws IOException
@@ -26,18 +45,29 @@ public class Program {
 	 * @throws ParserConfigurationException
 	 * @throws TransformerException
 	 * @throws TransformerFactoryConfigurationError
+	 * @throws org.apache.commons.cli.ParseException 
 	 */
 	public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException,
-			ParseException, TransformerFactoryConfigurationError, TransformerException {
-		// TODO read command line arguments
-		String inputDir = "/tmp/mychat";
-		String emojiDir = "/tmp/emojis";
-		String imagePoolDir = "/tmp/imagepool";
-
+			ParseException, TransformerFactoryConfigurationError, TransformerException, org.apache.commons.cli.ParseException {
+		// -i "/tmp/mychat" -e "/tmp/emojis" -imagepooldir "/tmp/imagepool"
+		
 		Config config = new Config();
-		config.setInputDir(inputDir);
-		config.setEmojiDir(emojiDir);
-		config.setImagePoolDir(imagePoolDir);
+		Options options=getOptions();
+		CommandLineParser parser=new DefaultParser();
+		try {
+			CommandLine line = parser.parse(options, args);
+			config.setInputDir(line.getOptionValue("inputdir"));
+			config.setEmojiDir(line.getOptionValue("emojidir"));
+			config.setOutputDir(line.getOptionValue("outputdir"));
+			config.setImagePoolDir(line.getOptionValue("imagepooldir"));
+			config.setDebugDir(line.getOptionValue("debugdir"));
+			
+		}
+		catch(MissingOptionException moe) {
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("thebook", options);
+			return;
+		}
 
 		config.setOutputDir(
 				Misc.isNullOrWhiteSpace(config.getOutputDir()) ? config.getInputDir() : config.getOutputDir());
