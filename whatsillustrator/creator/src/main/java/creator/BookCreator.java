1b9d52bb7a21bed182f08ce81018119e7e136fb5
Michael Elvers <melvers@gmail.com>
2021-12-29 23:32:15 +0100

    clean up

--- whatsillustrator/creator/src/main/java/creator/BookCreator.java
+++ whatsillustrator/creator/src/main/java/creator/BookCreator.java
@@ -1,36 +1,25 @@
 package creator;
 
 import helper.DateUtils;
-import helper.FileHandler;
-import imagematcher.*;
 
 import messageparser.*;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.Paths;
-import java.nio.file.Path;
 import java.text.ParseException;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import configurator.Global;
 import creator.plugins.IWriterPlugin;
-import creator.plugins.WriterConfig;
 import creator.plugins.WriterException;
 
 public class BookCreator {
 
-	//private final String DEFAULT_LOCALE = "en";
-
 	private static Logger logger = LogManager.getLogger(BookCreator.class);
 
-	//private WriterConfig config;
 	private Global globalConfig;
 	private IParser parser;
 	private List<IWriterPlugin> plugins;
@@ -41,99 +30,8 @@ public class BookCreator {
 		this.plugins = writerPlugins;
 	}
 
-	/*public void write_wa() throws IOException, ParseException, WriterException  {
-		List<String> txtFiles = FileHandler.listDir(this.config.getChatDir(), ".*.txt");
-		if (txtFiles.size() != 1) {
-			throw new IllegalArgumentException(
-					String.format("Invalid number of .txt-files found: %d", txtFiles.size()));
-		}
-
-		Path txtInputPath = Paths.get(txtFiles.get(0));
-		logger.info("Using {} as input", txtInputPath);
-
-		String namePrefix = txtInputPath.toFile().getName();
-		namePrefix = namePrefix.substring(0, namePrefix.length() - 4);
-		//config.setNamePrefix(namePrefix);
-
-		readProperties();
-		ImageMatcher im = getImageMatcher(namePrefix);
-		NameLookup nl = getNameLookup();
-
-		WhatsappParser parser = WhatsappParser.of(txtInputPath, im, nl);
-
-		// pre append
-		for (IWriterPlugin plugin : this.plugins) {
-			plugin.preAppend(this.config);
-		}
-
-		// write messages
-		logger.info("Start parsing messages");
-		IMessage msg;
-		LocalDateTime last = LocalDateTime.MIN;
-		while ((msg = parser.nextMessage()) != null) {
-			if (DateUtils.dateDiffer(last, msg.getTimepoint())) {
-				for (IWriterPlugin plugin : this.plugins) {
-					plugin.appendDateHeader(msg.getTimepoint());
-				}
-			}
-
-			last = msg.getTimepoint();
-
-			if (msg instanceof TextMessage) {
-				for (IWriterPlugin plugin : this.plugins) {
-					plugin.appendTextMessage((TextMessage) msg);
-				}
-			} else if (msg instanceof ImageMessage) {
-				for (IWriterPlugin plugin : this.plugins) {
-					plugin.appendImageMessage((ImageMessage) msg);
-				}
-			} else if (msg instanceof MediaOmittedMessage) {
-				for (IWriterPlugin plugin : this.plugins) {
-					plugin.appendMediaOmittedMessage((MediaOmittedMessage) msg);
-				}
-			} else if (msg instanceof MediaMessage) {
-				for (IWriterPlugin plugin : this.plugins) {
-					plugin.appendMediaMessage((MediaMessage) msg);
-				}
-			}
-		}
-		
-		// post append
-		for (IWriterPlugin plugin : this.plugins) {
-			plugin.postAppend();
-		}
-
-		im.toXmlFile();
-	}*/
 	
 	public void write() throws IOException, ParseException, WriterException  {
-		/*List<String> txtFiles = FileHandler.listDir(this.config.getChatDir(), ".*.txt");
-		if (txtFiles.size() != 1) {
-			throw new IllegalArgumentException(
-					String.format("Invalid number of .txt-files found: %d", txtFiles.size()));
-		}
-
-		Path txtInputPath = Paths.get(txtFiles.get(0));
-		logger.info("Using {} as input", txtInputPath);
-
-		String namePrefix = txtInputPath.toFile().getName();
-		namePrefix = namePrefix.substring(0, namePrefix.length() - 4);
-		config.setNamePrefix(namePrefix);*/
-
-		//readProperties();
-		/*ImageMatcher im = getImageMatcher(namePrefix);
-		NameLookup nl = getNameLookup();
-
-		WhatsappParser parser = WhatsappParser.of(txtInputPath, im, nl);*/
-		//config.setNamePrefix("prefix");
-		//Path txtInputPath = Paths.get(config.getInputDir().toString(), "bigfamily.json");
-		//TelegramParser parser = TelegramParser.of(txtInputPath);
-
-		// pre append
-		/*for (IWriterPlugin plugin : this.plugins) {
-			plugin.preAppend(this.config);
-		}*/
-
 		// write messages
 		logger.info("Start parsing messages");
 		IMessage msg;
@@ -174,71 +72,5 @@ public class BookCreator {
 		for (IWriterPlugin plugin : this.plugins) {
 			plugin.postAppend();
 		}
-
-		//im.toXmlFile();
 	}
-	
-	/*public WriterConfig getWriterConfig() {
-		return this.config;
-	}*/
-
-	/*private ImageMatcher getImageMatcher(String namePrefix) throws IOException, ParseException {
-		Path matchInputPath = this.globalConfig.getConfigDir().resolve(namePrefix + ".match.xml");
-		Path matchOutputPath = this.globalConfig.getOutputDir().resolve(namePrefix + ".match.xml");
-		ImageMatcher im = null;
-
-		if (matchInputPath.toFile().isFile()) {
-			logger.info("Loading matches '{}'", matchInputPath);
-			im = ImageMatcher.fromXmlFile(matchInputPath);
-			im.setSearchMode(false);
-		} else {
-			Path imagePoolDir = globalConfig.getImagePoolDir();
-			im = new ImageMatcher();
-			if (imagePoolDir == null) {
-				im.setSearchMode(false);
-			} else {
-				logger.info("Loading pool images from '{}'", imagePoolDir);
-				im.loadFiles(imagePoolDir);
-				im.setSearchMode(true);
-				logger.info("{} images found", im.getFileList().size());
-			}
-		}
-
-		im.setMatchOutputPath(matchOutputPath);
-
-		return im;
-	}*/
-
-	/*private NameLookup getNameLookup() throws IOException {
-		Path lookupInputPath = this.globalConfig.getConfigDir().resolve("namelookup.xml");
-		NameLookup nl;
-		if (lookupInputPath.toFile().isFile()) {
-			logger.info("Loading name lookup '{}'", lookupInputPath);
-			nl = NameLookup.fromXmlFile(lookupInputPath);
-		} else {
-			nl = new NameLookup();
-		}
-
-		return nl;
-	}*/
-
-	/*private void readProperties() throws FileNotFoundException, IOException {
-		Path propertiesInputPath = this.globalConfig.getConfigDir().resolve("bookcreator.properties");
-		
-		// file should contain
-		// locale=language
-		//String locale = this.DEFAULT_LOCALE;
-		boolean writeMediaOmittedHints = false;
-		if (propertiesInputPath.toFile().isFile()) {
-			logger.info("Using properties file '{}'", propertiesInputPath);
-			Properties properties = new Properties();
-			properties.load(new FileInputStream(propertiesInputPath.toFile()));
-
-			//locale = properties.getProperty("locale", locale);
-			writeMediaOmittedHints = Boolean.parseBoolean(
-					properties.getProperty("mediaomittedhints", Boolean.toString(writeMediaOmittedHints)));
-		}
-		//this.config.setDateUtils(new DateUtils(locale));
-		this.config.setWriteMediaOmittedHints(writeMediaOmittedHints);
-	}*/
 }
