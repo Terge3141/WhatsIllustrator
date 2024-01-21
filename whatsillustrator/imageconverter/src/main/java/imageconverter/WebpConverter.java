654a0eb217de1c9d947a327ebcc9afaef38db36d
Michael Elvers <melvers@gmail.com>
2024-01-21 19:25:20 +0100

    Implementation and unit tests

--- whatsillustrator/imageconverter/src/main/java/imageconverter/WebpConverter.java
+++ whatsillustrator/imageconverter/src/main/java/imageconverter/WebpConverter.java
@@ -1,10 +1,19 @@
 package imageconverter;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.nio.file.Path;
+
+import javax.imageio.ImageIO;
+
+import org.apache.commons.io.FilenameUtils;
 
 public class WebpConverter {
 	
-	public static void toPng(Path inputPath, Path outputDir) {
-		FilenameUtils.removeExtension(inputPath);
+	public static void toPng(Path inputPath, Path outputDir) throws IOException {
+		Path outputPath = outputDir.resolve(FilenameUtils.removeExtension(inputPath.toString()) + ".png");
+		
+		BufferedImage img = ImageIO.read(inputPath.toFile());
+		ImageIO.write(img, "png", outputPath.toFile());
 	}
 }
---
+++ whatsillustrator/imageconverter/src/test/java/imageconverter/WebpConverterTest.java
@@ -0,0 +1,97 @@
+package imageconverter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+import javax.imageio.ImageIO;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+class WebpConverterTest {
+
+	@Test
+	void testToPng(@TempDir Path tmpDir) throws IOException {
+		Path webpPath = tmpDir.resolve("mytest.webp");
+		Path pngExpectedPath = tmpDir.resolve("mytest.expected.png");
+		Path pngActualPath = tmpDir.resolve("mytest.png");
+
+		writeResource("/imageconverter/mytest.webp", webpPath);
+		writeResource("/imageconverter/mytest.png", pngExpectedPath);
+
+		WebpConverter.toPng(webpPath, tmpDir);
+		
+		assertTrue(pngActualPath.toFile().exists());
+		assertImagesEqual(pngExpectedPath, pngActualPath);
+	}
+	
+	private void assertImagesEqual(Path pngExpectedPath, Path pngActualPath) throws IOException {
+		BufferedImage imgExpected = ImageIO.read(pngExpectedPath.toFile());
+		BufferedImage imgActual = ImageIO.read(pngActualPath.toFile());
+
+		assertEquals(imgExpected.getHeight(), imgActual.getHeight());
+		assertEquals(imgExpected.getWidth(), imgActual.getWidth());
+		
+		double buf = 0.0;
+
+		for (int x = 0; x < imgExpected.getWidth(); x++) {
+			for (int y = 0; y < imgExpected.getHeight(); y++) {
+				int pixelExpected = imgExpected.getRGB(x, y);
+				int pixelActual = imgActual.getRGB(x, y);
+				buf += squaredRGBADiff(pixelExpected, pixelActual);
+			}
+		}
+		
+		// pixel values aren't 100% identical
+		buf = Math.sqrt(buf);
+		assertTrue(buf < 1500.);
+	}
+	
+	double squaredRGBADiff(int rgba1, int rgba2) {
+		int alpha1 = (rgba1 >> 24) & 255;
+        int red1 = (rgba1 >> 16) & 255;
+        int green1 = (rgba1 >> 8) & 255;
+        int blue1 = rgba1 & 255;
+        
+        int alpha2 = (rgba2 >> 24) & 255;
+        int red2 = (rgba2 >> 16) & 255;
+        int green2 = (rgba2 >> 8) & 255;
+        int blue2 = rgba2 & 255;
+        
+        double buf = 0.0;
+        buf += squaredDiff(alpha1, alpha2);
+        buf += squaredDiff(red1, red2);
+        buf += squaredDiff(green1, green2);
+        buf += squaredDiff(blue1, blue2);
+        
+        return buf;
+	}
+	
+	double squaredDiff(int a, int b) {
+		double ad = (double)a;
+		double bd = (double)b;
+		
+		return (ad-bd)*(ad-bd);
+	}
+	
+	void dump(int rgba) {
+		int alpha = (rgba >> 24) & 255;
+        int red = (rgba >> 16) & 255;
+        int green = (rgba >> 8) & 255;
+        int blue = rgba & 255;
+        
+        System.out.println("\t alpha " + alpha);
+        System.out.println("\t red   " + red);
+        System.out.println("\t green " + green);
+        System.out.println("\t blue  " + blue);
+	}
+
+	void writeResource(String src, Path dst) throws IOException {
+		Files.copy(getClass().getResourceAsStream(src), dst, StandardCopyOption.REPLACE_EXISTING);
+	}
+}
